d5bc548290dfb5fd8ef8a9822133ca07112407d2
Payam Abdullah <payam97abubakr@gmail.com>
2021-06-16 20:03:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,19 +57,6 @@ let wagesEarnedOnDate = function(dateSought){
     return parseFloat(rawWage.toString())
 }
 
-let allWagesFor = function(){
-    let eligibleDates = this.timeInEvents.map(function(e){
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function (memo, d) {
-    let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-    }.bind(this), 0)
-
-    return payable
-}
 
 let findEmployeeByFirstName = function(srcArray, firstName) {
   return srcArray.find(function(rec){
@@ -81,4 +68,28 @@ let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
         return memo + allWagesFor.call(rec)
     }, 0)
+}
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+@@ -8,15 +78,15 @@
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+ 
+ let allWagesFor = function(){
+    let eligibleDates = this.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0)
+
+    return payable
 } 
